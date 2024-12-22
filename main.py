15504b883b727b8838b1eab88b8a858aabfd7073
Uzair <uhass924@gmail.com>
2024-12-21 23:34:27 -0500

    m

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-import json, names
-from transformers import pipeline
+import names, json
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
-# File names.py should contain a list names[] of full names 
 names = names.names
+model = AutoModelForCausalLM.from_pretrained("microsoft/phi-2", torch_dtype="auto", trust_remote_code=True)
+tokenizer = AutoTokenizer.from_pretrained("microsoft/phi-2", trust_remote_code=True)
 
-messages = [
-    {"role": "system", "content": "I will give you a few full names, and you will tell me how does each name sound by describing the name in a few words (less than 5). Then you will tell me what emotion or feeling does it invoke."}
-]
-for name in names:
-    message = {"role": "user", "content": f"name"}
-    messages.append(message)
+inputs = tokenizer(f"Following is a list of a person's given names.{str(names)}. Describe each name in a few words (less than 5). What feeling or emotions does it invoke?"
+, return_tensors="pt", return_attention_mask=False)
 
-chatbot = pipeline("text-generation", model="mistralai/Mistral-7B-Instruct-v0.3")
-chatbot(messages)
+outputs = model.generate(**inputs, max_length=200)
+text = tokenizer.batch_decode(outputs)[0]
+print(text)
+
 
